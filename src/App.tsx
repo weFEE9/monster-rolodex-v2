497813bff9f701013c97f3ff988b205b5d0cefbc
weeee9 <teletubby332@gmail.com>
2024-01-24 21:17:15 +0800

    feat: searching monsters

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ type Monster = {
 };
 
 const App = () => {
+  const [searchField, setSearchField] = React.useState('');
   const [monsters, setMonsters] = React.useState<Monster[]>([]);
+  const [filteredMonsters, setFilteredMonsters] = React.useState<Monster[]>([]);
 
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -16,9 +18,30 @@ const App = () => {
       .then((users: Monster[]) => setMonsters(users));
   }, []);
 
+  React.useEffect(() => {
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField)
+    );
+
+    setFilteredMonsters(filteredMonsters);
+  }, [monsters, searchField]);
+
+  const handleSearchFieldChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = event.target.value.toLocaleLowerCase();
+    setSearchField(value);
+  };
+
   return (
     <div className='App'>
-      {monsters.map((monster) => {
+      <input
+        className='search-box'
+        type='search'
+        placeholder='Search monsters'
+        onChange={handleSearchFieldChange}
+      />
+      {filteredMonsters.map((monster) => {
         return (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
